f12244778ed49df890422883c3bf551b689676b6
Martijn Reede <75437878+MartijnReede@users.noreply.github.com>
2021-02-28 17:08:50 +0100

    Update RecursionContinued.java

--- Job Ready Programmer Course/Practical Data Structures and Algorithms/Lesson 15 - Recursion Algorithm/RecursionContinued.java
+++ Job Ready Programmer Course/Practical Data Structures and Algorithms/Lesson 15 - Recursion Algorithm/RecursionContinued.java
@@ -6,7 +6,11 @@ public class RecursionContinued {
 		
 		reduceByOne(10);
 		
-		//For the method reduceByOne(int n) the n == 0 condition is called the BASE CASE. 		
+		//For the method reduceByOne(int n) the n == 0 condition is called the BASE CASE. 
+		//As you can see from the printed results, the first result that is going to print is -1 while
+		//you maybe would expect that 10 would print first. This is because when we first invoke the method, it never
+		//reaches the println method. Only when n < 0 it reaches the the println method and this is when n == -1.
+	
 	}
 	
 	public static void reduceByOne(int n) {
