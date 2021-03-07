1081d4995619b81d937efa7580498bd84ccd0d90
Martijn Reede <75437878+MartijnReede@users.noreply.github.com>
2021-03-07 13:03:48 +0200

    Add files via upload

---
+++ Job Ready Programmer Course/Learn Spring 5, Boot 2, JPA Thymeleaf, AOP, Web MVC, REST/Lesson 01 - Introduction to Spring/Application.java
@@ -0,0 +1,23 @@
+package com.spring.introToSpring;
+
+public class Application {
+	
+	//SPRING: The original intent of the spring framework was to make developing java applications easier. 
+	
+	//FRAMEWORK: Supports multiple languages. 
+	
+	//WHAT DOES IT PROVIDE: Every software application has various infrastructural requirements. For example: the application 
+	//must connect to a database or connect with an external API. Connections and passwords need to be configured and 
+	//also how your application is going to run on different platforms. Doing this in Java alone is a nightmare, Spring provides
+	//this infrastructural support on the application level. 
+	
+	//SPRING BOOT: Is the icing on the cake... Spring Boot is a component of the Spring Framework that can kick start your
+	//application in minutes. Spring Boot uses various defaults for you that comply with best practices in developing various
+	//kinds of applications. We don't have to worry about what is the best way to create an application. 
+	
+	
+	
+	
+	
+	
+}
