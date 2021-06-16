efe2f202b0d197687fd523efe054d123a0272be9
Martijn Reede <75437878+MartijnReede@users.noreply.github.com>
2021-06-16 12:12:33 +0200

    Delete Project 11 - JavaFX Mastermind directory

--- Project 11 - JavaFX Mastermind/Source Code/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module MasterMind {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-	requires javafx.graphics;
-    opens MasterMind.MasterMind to javafx.fxml;
-    exports MasterMind.MasterMind;
-}
